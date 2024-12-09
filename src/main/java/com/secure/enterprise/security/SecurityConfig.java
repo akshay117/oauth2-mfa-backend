6a7a79efbe4cd359e09b54d204f4e68a8c87d0c7
Akshay V Anil <30944903+akshay117@users.noreply.github.com>
2024-12-10 02:16:23 +0530

    Added csrf protection

--- src/main/java/com/secure/enterprise/security/SecurityConfig.java
+++ src/main/java/com/secure/enterprise/security/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 
 import java.time.LocalDate;
@@ -29,11 +30,17 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity(prePostEnabled = true,securedEnabled = true , jsr250Enabled = true )
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, CustomLoggingFilter customLoggingFilter) throws Exception {
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+         //enabled csrf protection here
+        http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringRequestMatchers("/api/auth/public/**"));
+
+        // http.csrf(AbstractHttpConfigurer::disable); this disables csrf protection
+
         http.authorizeHttpRequests((requests) -> requests
                // .requestMatchers("/api/admin/**").hasRole("ADMIN")  Either use this or method level security for role based access
+                .requestMatchers("/api/csrf-token").permitAll()
                 .anyRequest().authenticated());
-        http.csrf(AbstractHttpConfigurer::disable);
       //  http.addFilterBefore(new CustomLoggingFilter(), UsernamePasswordAuthenticationFilter.class);
      //   http.addFilterAfter(new RequestValidationFilter(),  CustomLoggingFilter.class);
         //http.formLogin(withDefaults());
